fed1359479f73397f1ec51b5191fc4aff029e5df
CanhDuc710 <canhvdhe170639@fpt.edu.vn>
2025-05-16 15:49:48 +0700

    create project

---
+++ se1979-su25-g2-be/se1979-su-25-g2-be/src/main/java/org/example/se1979su25g2be/Se1979Su25G2BeApplication.java
@@ -0,0 +1,13 @@
+package org.example.se1979su25g2be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se1979Su25G2BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se1979Su25G2BeApplication.class, args);
+    }
+
+}
---
+++ se1979-su25-g2-be/se1979-su-25-g2-be/src/test/java/org/example/se1979su25g2be/Se1979Su25G2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.se1979su25g2be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se1979Su25G2BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
