47fa20f8623e7b5bb3cb0d3c8a9d0f855fad0151
ridot <hungnmhe171137@fpt.edu.vn>
2025-06-05 23:38:53 +0700

    creat faq

--- se1979-su25-g2/se1979-su-25-g2-be/src/main/java/org/example/se1979su25g2be/entity/FAQ.java
+++ se1979-su25-g2/se1979-su-25-g2-be/src/main/java/org/example/se1979su25g2be/entity/FAQ.java
@@ -1,4 +1,16 @@
 package org.example.se1979su25g2be.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class FAQ {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+    private String answer;
 }
