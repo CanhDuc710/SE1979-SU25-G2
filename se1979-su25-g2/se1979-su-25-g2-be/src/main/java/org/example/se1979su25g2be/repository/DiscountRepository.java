8cd61d5624d290e417dd82168f1085e622188fa6
Tung <128894003+TungNSHE172510@users.noreply.github.com>
2025-06-17 15:50:33 +0700

    discount repos

---
+++ se1979-su25-g2/se1979-su-25-g2-be/src/main/java/org/example/se1979su25g2be/repository/DiscountRepository.java
@@ -0,0 +1,18 @@
+package org.example.se1979su25g2be.repository;
+
+import org.example.se1979su25g2be.entity.Discount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public interface DiscountRepository extends JpaRepository<Discount, Integer> {
+    Optional<Discount> findByCode(String code);
+
+    @Query("SELECT d FROM Discount d WHERE " +
+            "(:min IS NULL OR d.minOrderValue >= :min) AND " +
+            "(:max IS NULL OR d.maxDiscountAmount <= :max)")
+    List<Discount> filterByValue(BigDecimal min, BigDecimal max);
+}
