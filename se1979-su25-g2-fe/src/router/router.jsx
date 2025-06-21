a758ac2f56d1b839cd9301f9f80f264544a00b49
ducnda <ducndahe171559@fpt.edu.vn>
2025-06-21 11:04:47 +0700

    edit internal account

--- se1979-su25-g2-fe/src/router/router.jsx
+++ se1979-su25-g2-fe/src/router/router.jsx
@@ -8,7 +8,9 @@ const ProductManagementLazy = lazy(() => import("../page/admin/product/ProductMa
 const NotFoundLazy = lazy(() => import("../page/error/NotFound.jsx"));
 const ProductDetailLazy = lazy(() => import("../page/Product/ProductDetail.jsx"));
 const UserListLazy = lazy(() => import("../page/admin/user_management/UserList.jsx"));
+const AddAccountLazy = lazy(() => import("../page/admin/user_management/AddInternalAccount.jsx"));
 const UserDetailsLazy = lazy(() => import("../page/admin/user_management/UserDetail.jsx"));
+const EditInternalAccount = lazy(() => import("../page/admin/user_management/EditInternalAccount.jsx"));
 const OrderPageLazy = lazy(() => import("../page/Order/OrderPage.jsx"));
 const CartPageLazy = lazy(() => import("../page/Cart/CartPage.jsx"));
 const router = createBrowserRouter([
@@ -20,6 +22,8 @@ const router = createBrowserRouter([
             { path: "/products/:productId", element: <ProductDetailLazy /> }, // Thêm dòng này
             { path: "/admin/accounts", element: <UserListLazy /> },
             { path:"/admin/accounts/:id", element:<UserDetailsLazy />},
+            { path:"/admin/accounts/edit/:id", element:<EditInternalAccount />},
+            { path:"/admin/accounts/add", element:<AddAccountLazy />},
             { path: "*", element: <NotFoundLazy /> },
             { path: "/admin/products", element: <ProductManagementLazy /> },
             { path: "/cart", element: <CartPageLazy /> },
